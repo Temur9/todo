505231fbc818fb2f08a7eba2da63c3af0f336573
Temur9 <99079769+Temur9@users.noreply.github.com>
2022-09-29 23:34:39 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import Header from "./components/Header";
+import "./App.css";
+import Form from "./components/Form";
+import { useEffect, useState } from "react";
+import TodosList from "./components/TodosList";
+function App() {
+  const initialState = JSON.parse(localStorage.getItem("todos")) || [];
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState(initialState);
+  // const [todos, setTodos] = useState([]);-------> without localstorage
+  const [editTodo, setEditTodo] = useState(null);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+  return (
+    <>
+      <div className="container">
+        <div className="app-wrapper">
+          <div>
+            <Header />
+          </div>
+          <div>
+            <Form
+              input={input}
+              setInput={setInput}
+              todos={todos}
+              setTodos={setTodos}
+              editTodo={editTodo}
+              setEditTodo={setEditTodo}
+            />
+          </div>
+          <div>
+            <TodosList
+              todos={todos}
+              setTodos={setTodos}
+              setEditTodo={setEditTodo}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
